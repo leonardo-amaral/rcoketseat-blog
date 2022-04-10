251614a64ef2bf5c78d3b0939b2ecd82b0db40b9
Leonardo Amaral <leozero103@gmail.com>
2022-04-10 20:48:22 -0300

    Content

--- src/components/Content - 1/Index.jsx
+++ src/components/Content - 1/Index.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import '../../styles/Content.css'
+import Image2 from '../../assets/featured-image.svg'
+import Arrow from '../../assets/arrow-right.svg'
 
 function Index() {
   return (
     <div>
-      <section>
-        <h1>Veja o guia definitivo para conquistar seus objetivos como DEV em 2022</h1>
+      <section className='content-page1'>
+        <div className='text'>
+          <h1 className='text-content'>Veja o guia definitivo para conquistar seus objetivos como DEV em 2022</h1>
+          <h1 className='p-content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh nibh eu in aliquet ut adipiscing neque. Sed volutpat aenean sit vitae, sed tristique placerat hac. </h1>
+          <div>
+            <h2 className='btn-text'>Veja mais <img className='image-content' src={Arrow} alt="image" /></h2>
+          </div>
+        </div>
+        <div className="image">
+          <img className='image-content' src={Image2} alt="image" />
+        </div>
       </section>
     </div>
   )
